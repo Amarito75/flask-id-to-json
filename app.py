73dcaa40a643050f4b6b236e3bdf9d15c09d067c
Amarito75 <a.matallah767@gmail.com>
2023-08-17 20:51:08 +0200

    :tada: first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def ajouter_ids_dans_json():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': 'Aucun fichier téléchargé'})
+
+        fichier = request.files['file']
+        if fichier.filename == '':
+            return jsonify({'error': 'Aucun fichier sélectionné'})
+
+        data = json.load(fichier)
+
+        modified_data = []
+        for idx, entry in enumerate(data, start=1):
+            entry_dict = {'id': idx, **entry}
+            modified_data.append(entry_dict)
+
+        modified_json = json.dumps(modified_data, indent=4)
+
+        return jsonify({'modified_json': modified_json})
+
+    except Exception as e:
+        return jsonify({'error': f'Une erreur s\'est produite : {e}'})
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
